ab72d542d31852e33093f7512d0d63690bf29a3a
eesoymilk <qiyoudaoyi@gmail.com>
2024-01-17 17:37:45 +0800

    Add: simple i18n

---
+++ src/i18n/translations.ts
@@ -0,0 +1,12 @@
+export default {
+  en: {
+    'header.title': 'Hello, World!',
+    'header.name': 'Hi <strong>{{name}}</strong>, how are you?',
+    'homepage.time': 'The current time is: {{time}}',
+  },
+  es: {
+    'homepage.title': '¡Hola Mundo!',
+    'homepage.welcome': 'Hola, <strong>{{name}}</strong>, ¿cómo estás?',
+    'homepage.time': 'La hora actual es: {{time}}',
+  },
+} as const;
