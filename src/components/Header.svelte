4b358f8fbbfbc8a1a30438e971623bf0f04e2ff8
qiyoudaoyi <qiyoudaoyi@gmail.com>
2024-01-24 00:28:37 +0800

    Add: gradient bg and rainbow avatar

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -4,12 +4,14 @@
   const titles = ['software developer', 'programming tutor', 'IoT enthusiast'];
 </script>
 
-<header class="flex flex-col mx-auto w-2/3 h-screen font-display">
+<header
+  class="mx-auto flex h-screen flex-col bg-gradient-to-b from-sky-700 from-40% to-sky-100 font-display"
+>
   <div
-    class="flex flex-col flex-1 gap-4 justify-center items-center min-h-0 md:gap-6 lg:gap-8 basis-auto"
+    class="mx-auto flex min-h-0 w-2/3 flex-1 basis-auto flex-col items-center justify-center gap-4 md:gap-6 lg:gap-8"
   >
     <div class="flex flex-col flex-wrap gap-1">
-      <div class="flex gap-x-1 items-end">
+      <div class="flex items-end gap-x-1">
         <span class="text-lg font-medium md:text-xl xl:text-2xl">Hi, I am</span>
         <h1 class="text-2xl font-extrabold md:text-3xl xl:text-4xl">
           Yu-Wei Chang
@@ -28,11 +30,14 @@
         {/each}
       </ul>
     </div>
-    <div>
+    <div class="relative">
+      <div
+        class="animate-rainbow absolute inset-0 h-full w-full rounded-full drop-shadow"
+      ></div>
       <img
         src={avatar.src}
         alt="avatar"
-        class="mx-auto w-full rounded-full ring-8 max-w-[40vh] ring-sky-700 drop-shadow-lg sm:w-[427px]"
+        class="mx-auto w-full max-w-[40vh] rounded-full p-2 drop-shadow-lg sm:w-[427px] md:p-4"
       />
     </div>
   </div>
