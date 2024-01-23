709c880661142b6cdf9029758eeda6df0185df22
eesoymilk <qiyoudaoyi@gmail.com>
2024-01-23 18:24:59 +0800

    Add: about me

---
+++ src/components/About.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import Title from './Title.svelte';
+
+  const paragraphs = [
+    'I am currently a senior student at National Tsing Hua University majoring Eletrical Engineering with a keen interest in the field of machine learning and computer vision. Feel free to contact me for any software development or tutoring opportunities.',
+  ];
+</script>
+
+<Title>About Me</Title>
+{#each paragraphs as paragraph}
+  <p class="px-4 mx-auto max-w-5xl font-sans md:text-lg">{paragraph}</p>
+{/each}
--- src/components/Title.svelte
+++ src/components/Title.svelte
@@ -1,6 +1,6 @@
-<div class="overflow-hidden relative">
+<div class="overflow-hidden relative p-4">
   <h2
-    class="font-display text-center text-xl font-bold leading-loose text-sky-800 before:absolute before:top-[calc(100%/3)] before:h-1/3 before:w-screen before:-translate-x-[calc(100%+1rem)] before:bg-sky-700 before:content-[''] after:absolute after:top-[calc(100%/3)] after:h-1/3 after:w-screen after:translate-x-[1rem] after:bg-sky-700 after:content-[''] sm:text-2xl md:text-3xl xl:text-4xl"
+    class="text-center font-display text-xl font-bold leading-loose text-sky-800 before:absolute before:top-[calc(300%/8)] before:h-1/4 before:w-screen before:-translate-x-[calc(100%+1rem)] before:bg-sky-700 before:content-[''] after:absolute after:top-[calc(300%/8)] after:h-1/4 after:w-screen after:translate-x-[1rem] after:bg-sky-700 after:content-[''] sm:text-2xl md:text-3xl xl:text-4xl"
   >
     <slot>Title</slot>
   </h2>
