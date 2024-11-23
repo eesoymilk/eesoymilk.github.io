0a3d7d55a57d05dfee0a4a7949f84697c2a222e6
qiyoudaoyi <qiyoudaoyi@gmail.com>
2024-11-22 21:54:09 -0800

    feat: default to dark theme

--- src/stores/theme.ts
+++ src/stores/theme.ts
@@ -1,49 +1,49 @@
-import { derived, writable } from 'svelte/store';
+import { derived, writable } from "svelte/store";
 
-type LightOrDark = 'light' | 'dark';
+type LightOrDark = "light" | "dark";
 
 const themeStore = () => {
-  const store = writable<LightOrDark>('light');
-  const { subscribe, set, update } = store;
+	const store = writable<LightOrDark>("dark");
+	const { subscribe, set, update } = store;
 
-  const changeClass = (mode: LightOrDark) => {
-    if (mode === 'dark') {
-      document.documentElement.classList.add('dark');
-    } else {
-      document.documentElement.classList.remove('dark');
-    }
-    !import.meta.env.SSR && localStorage.setItem('theme', mode);
-    return mode;
-  };
+	const changeClass = (mode: LightOrDark) => {
+		if (mode === "dark") {
+			document.documentElement.classList.add("dark");
+		} else {
+			document.documentElement.classList.remove("dark");
+		}
+		!import.meta.env.SSR && localStorage.setItem("theme", mode);
+		return mode;
+	};
 
-  return {
-    subscribe,
-    init: () => {
-      try {
-        const saved =
-          localStorage.getItem('theme') ||
-          window.matchMedia('(prefers-color-scheme: dark)').matches
-            ? 'dark'
-            : 'light';
-        if (saved === 'dark' || saved === 'light') {
-          changeClass(saved);
-          set(saved);
-        }
-      } catch (e) {
-        if (e instanceof ReferenceError) {
-          console.error('localStorage is not available in SSR');
-        }
-      }
-    },
-    toggle: () => {
-      update((mode) => changeClass(mode === 'dark' ? 'light' : 'dark'));
-    },
-  };
+	return {
+		subscribe,
+		init: () => {
+			try {
+				const saved =
+					localStorage.getItem("theme") ||
+					window.matchMedia("(prefers-color-scheme: dark)").matches
+						? "dark"
+						: "light";
+				if (saved === "dark" || saved === "light") {
+					changeClass(saved);
+					set(saved);
+				}
+			} catch (e) {
+				if (e instanceof ReferenceError) {
+					console.error("localStorage is not available in SSR");
+				}
+			}
+		},
+		toggle: () => {
+			update((mode) => changeClass(mode === "dark" ? "light" : "dark"));
+		},
+	};
 };
 
 export const theme = themeStore();
 export const themeIcon = derived(theme, ($theme) =>
-  $theme === 'light'
-    ? 'material-symbols:light-mode-outline'
-    : 'material-symbols:dark-mode-outline'
+	$theme === "light"
+		? "material-symbols:light-mode-outline"
+		: "material-symbols:dark-mode-outline",
 );
